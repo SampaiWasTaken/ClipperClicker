5ed128144a63c4b584984014012ee21cfa5ceba7
MrMerlinH <leprueller@edu.aau.at>
2023-07-27 14:53:43 +0200

    Added documentation for Main and IdleScreenShakeAnimator

--- app/src/main/java/com/example/appdevprojectradlerprueller/IdleScreenShakeAnimator.kt
+++ app/src/main/java/com/example/appdevprojectradlerprueller/IdleScreenShakeAnimator.kt
@@ -7,19 +7,40 @@ import android.animation.ObjectAnimator
 import android.view.View
 import kotlin.math.sin
 
+/**
+ * IdleScreenShakeAnimator is a utility class that provides a method to create an AnimatorSet
+ * that applies a screen shake animation to a given view with random displacement along the
+ * X and Y axes.
+ */
 class IdleScreenShakeAnimator {
-    fun getScreenShakeAnimatorSet(amplitude:Float, view: View):AnimatorSet{
-        val transX = ObjectAnimator.ofFloat(view, "translationX",(sin((Math.random() * (628))) * amplitude).toFloat() ).apply { duration = 1000 }
+
+    /**
+     * Generates an AnimatorSet that creates a screen shake effect for the specified view.
+     *
+     * @param amplitude The amplitude of the screen shake effect. The higher the amplitude, the more
+     *                  pronounced the shake will be.
+     * @param view The view that will be animated with the screen shake effect.
+     * @return An AnimatorSet that applies the screen shake animation to the given view.
+     */
+    fun getScreenShakeAnimatorSet(amplitude: Float, view: View): AnimatorSet {
+        // Generate random displacement along the X and Y axes using sine function with the given amplitude.
+        val transX = ObjectAnimator.ofFloat(view, "translationX", (sin((Math.random() * (628))) * amplitude).toFloat()).apply { duration = 1000 }
         val transY = ObjectAnimator.ofFloat(view, "translationY", (sin((Math.random() * (628))) * amplitude).toFloat()).apply { duration = 1000 }
-        val animatorset = AnimatorSet().apply {
+
+        // Create an AnimatorSet to combine both X and Y translations in parallel.
+        val animatorSet = AnimatorSet().apply {
             play(transX).with(transY)
         }
-        animatorset.addListener(object : AnimatorListenerAdapter() {
+
+        // Add an AnimatorListenerAdapter to the AnimatorSet, which restarts the animation
+        // when it ends, creating an infinite looping effect.
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator) {
                 super.onAnimationEnd(animation)
-                getScreenShakeAnimatorSet(amplitude,view).start()
+                getScreenShakeAnimatorSet(amplitude, view).start()
             }
         })
-        return animatorset
+
+        return animatorSet
     }
 }
